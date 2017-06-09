75d357f55b4f633bf6fff357ce65daa5cdb7790e
Robert Pirritano <rpirritano@gmail.com>
2017-06-09 16:53:16 -0700

    mentor availability: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,4 +16,9 @@ class Kele
         response = self.class.get("/users/me", headers: {"authorization" => @auth_token })
         user_data = JSON.parse(response.body)
     end
+
+    def get_mentor_availability(id)
+		response = self.class.get("/mentors/#{id}/student_availability", headers: { "authorization" => @auth_token } )	
+		@mentor_availability = JSON.parse(response.body)
+	end
 end
