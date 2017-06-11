57131a6844516aef5975fb3728369f6d9aec9488
Robert Pirritano <rpirritano@gmail.com>
2017-06-11 10:26:02 -0700

    checkpoint submission:completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -43,4 +43,19 @@ class Kele
           "stripped-text": stripped_text
         })
   end
+  
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+      response = self.class.post("/checkpoint_submissions", headers: { "authorization" => @auth_token },	
+      body: {
+          "checkpoint_id": checkpoint_id,
+          "assignment_branch": assignment_branch,
+          "assignment_commit_link": assignment_commit_link,
+          "comment": comment,
+          "enrollment_id": enrollment_id
+      })
+  end
+
+
+
+
 end
